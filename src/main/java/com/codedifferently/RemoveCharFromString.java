ca8d44760178028b4030ebf8cec486e89e7d2712
ahmadthegoat <ahcon1@morgan.edu>
2020-08-12 11:42:58 -0400

    Done i think

--- src/main/java/com/codedifferently/RemoveCharFromString.java
+++ src/main/java/com/codedifferently/RemoveCharFromString.java
@@ -1,17 +1,29 @@
 package com.codedifferently;
 
-import com.sun.org.slf4j.internal.Logger;
-import com.sun.org.slf4j.internal.LoggerFactory;
+//import com.sun.org.slf4j.internal.Logger;
+//import com.sun.org.slf4j.internal.LoggerFactory;
 
 public class RemoveCharFromString {
-    private static final Logger logger = LoggerFactory.getLogger(RemoveCharFromString.class);
+  //  private static final Logger logger = LoggerFactory.getLogger(RemoveCharFromString.class);
 
     public String remove(String word, char unwanted){
-        return null;
+        String[] letters = word.split("");
+        String result = "";
+        for(String letter: letters){
+            if(!letter.equals(Character.toString(unwanted))){
+            result += letter;
+            }}
+        return result;
     }
 
     /*Extra Credit Flex*/
     public String removeRecursive(String word, char ch){
-        return null;
+        int unwantedindex = word.indexOf(Character.toString(ch));
+        if(unwantedindex > word.length() || unwantedindex < 0 ){
+            return word;
+        }
+        else{
+            String newword = word.substring(0,unwantedindex) + word.substring(unwantedindex+1, word.length());
+        return removeRecursive(newword, ch);
     }
-}
+}}
